f3283f0e2b78d6cb294b00a842d06422ab3adf5d
alee12131415 <alee12131415@gmail.com>
2016-08-10 03:10:43 -0700

    completed creating tables

--- app/src/main/java/com/example/symphony/phrx/DatabaseHandler.java
+++ app/src/main/java/com/example/symphony/phrx/DatabaseHandler.java
@@ -1,8 +1,11 @@
 package com.example.symphony.phrx;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.symphony.phrx.db_classes.PersonalHealth;
 
 /**
  * Created by Andrew on 8/9/2016.
@@ -25,8 +28,31 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final String PH_SYS = "systolic";
     private static final String PH_DIA = "diastolic";
     private static final String PH_HR = "heart_rate";
-    //
-
+    //medication fields
+    private static final String MED_ID ="id";
+    private static final String MED_NAME = "name";
+    private static final String MED_DOSE = "dose";
+    private static final String MED_DOSE_UNIT = "dose_unit";
+    private static final String MED_DOSAGE = "dosage";
+    private static final String MED_DOSAGE_UNIT = "dosage_unit";
+    private static final String MED_FREQ = "frequency";
+    private static final String MED_FREG_INV = "frequency_interval";
+    private static final String MED_ADMIN = "administration";
+    private static final String MED_REASON = "reason";
+    //immunization fields
+    private static final String IMM_ID = "id";
+    private static final String IMM_NAME = "name";
+    private static final String IMM_DATE = "date";
+    private static final String IMM_DOSE = "dose"; //dose number, calculate updates somewhere else
+    //allergy fields
+    private static final String ALL_ID = "id";
+    private static final String ALL_NAME = "name";
+    private static final String ALL_SYM = "symptom";
+    private static final String ALL_MEDI = "medication";
+    //condition fields
+    private static final String CON_ID = "id";
+    private static final String CON_NAME = "name";
+    private static final String CON_DESC = "description";
 
     //create table statements
     private static final String CREATE_TABLE_PERSONAL_HEALTH = "CREATE TABLE " + TABLE_PERSONAL_HEALTH +
@@ -38,7 +64,41 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             PH_DIA + " INT," +
             PH_HR + " INT" +
             ")";
+    private static final String CREATE_TABLE_MEDICATION = "CREATE TABLE " + TABLE_MEDICATION +
+            "(" +
+            MED_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            MED_NAME + " TEXT," +
+            MED_DOSE + " REAL," +
+            MED_DOSE_UNIT + " TEXT," +
+            MED_DOSAGE + " REAL," +
+            MED_DOSAGE_UNIT + " TEXT," +
+            MED_FREQ + " REAL," +
+            MED_FREG_INV + " TEXT," +
+            MED_ADMIN + " TEXT," +
+            MED_REASON + " TEXT" +
+            ")";
+    private static final String CREATE_TABLE_IMMUNIZATION = "CREATE TABLE " + TABLE_IMMUNIZATION +
+            "(" +
+            IMM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            IMM_NAME + " TEXT," +
+            IMM_DATE + " TEXT," +
+            IMM_DOSE + " INT" +
+            ")";
+    private static final String CREATE_TABLE_ALLERGY = "CREATE TABLE " + TABLE_ALLERGY +
+            "(" +
+            ALL_ID + " INTEGER PRIMATE KEY AUTOINCREMENT," +
+            ALL_NAME + " TEXT," +
+            ALL_SYM + " TEXT," +
+            ALL_MEDI + " TEXT" +
+            ")";
+    private static final String CREATE_TABLE_CONDITION = "CREATE TABLE " + TABLE_CONDITION +
+            "(" +
+            CON_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            CON_NAME + " TEXT," +
+            CON_DESC + " TEXT" +
+            ")";
 
+    //constructor
     public  DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -46,13 +106,55 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE_PERSONAL_HEALTH);
+        db.execSQL(CREATE_TABLE_MEDICATION);
+        db.execSQL(CREATE_TABLE_IMMUNIZATION);
+        db.execSQL(CREATE_TABLE_ALLERGY);
+        db.execSQL(CREATE_TABLE_CONDITION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVerseion) {
         if (oldVersion != newVerseion) {
             db.execSQL("DROP TABLE IF EXISTS " + TABLE_PERSONAL_HEALTH);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_MEDICATION);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_IMMUNIZATION);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_ALLERGY);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONDITION);
             onCreate(db);
         }
+    }
+
+    //methods
+
+    //personal health
+    public void createPersonalHealth(PersonalHealth ph) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(PH_WEIGHT, ph.getWeight());
+        values.put(PH_HEIGHT, ph.getHeight());
+        values.put(PH_SYS, ph.getSystolic());
+        values.put(PH_DIA, ph.getSystolic());
+        values.put(PH_HR, ph.getHeartRate());
+
+        long PersonalHealth_return = db.insert(TABLE_PERSONAL_HEALTH, null, values); //row id returned //-1 if error
+    }
+    //untested edit method, please check
+    public void editPersonalHealth (PersonalHealth ph, int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(PH_WEIGHT, ph.getWeight());
+        values.put(PH_HEIGHT, ph.getHeight());
+        values.put(PH_SYS, ph.getSystolic());
+        values.put(PH_DIA, ph.getSystolic());
+        values.put(PH_HR, ph.getHeartRate());
+
+        long PersonalHealth_returns = db.update(TABLE_PERSONAL_HEALTH, values, "_id=" + id, null);
+    }
+    public void deletePersonalHealth(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.delete(TABLE_PERSONAL_HEALTH, PH_ID + " = ?", new String[] {String.valueOf(id)});
     }
 }
---
+++ app/src/main/java/com/example/symphony/phrx/db_classes/PersonalHealth.java
@@ -0,0 +1,56 @@
+package com.example.symphony.phrx.db_classes;
+
+/**
+ * Created by Andrew on 8/10/2016.
+ */
+public class PersonalHealth {
+    private int id;
+    private double weight;
+    private double height;
+    private int systolic;
+    private int diastolic;
+    private int heartRate;
+
+    //set
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setSystolic(int systolic) {
+        this.systolic = systolic;
+    }
+    public void setDiastolic(int diastolic) {
+        this.diastolic = diastolic;
+    }
+    public void setHeartRate(int heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    //get
+    public int getId() {
+        return id;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public int getSystolic() {
+        return systolic;
+    }
+    public int getDiastolic() {
+        return diastolic;
+    }
+    public int getHeartRate() {
+        return heartRate;
+    }
+}
